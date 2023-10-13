eff79fcb61a2db65a7c798cb0225a5a652570e74
rex-9 <htetnaing0814@gmail.com>
2023-10-13 10:15:11 +0630

    update

---
+++ Hackerrank/ProblemSolving/birthday.js
@@ -0,0 +1,9 @@
+function birthday(s, d, m) {
+    // Write your code here
+    for (let i = 0; i < m; i++) {
+        
+    }
+    return 0;
+}
+
+console.log(birthday([2, 2, 1, 3, 2], 4, 2));
---
+++ Hackerrank/ProblemSolving/sumPairs.js
@@ -0,0 +1,30 @@
+function divisibleSumPairs(n, k, ar) {
+    // Write your code here
+    const result = [];
+    for (let i = 0; i < n; i++) {
+        let j = ar.filter((x, index) => index > i && (x + ar[i]) % k === 0);
+        if (j.length > 0) {
+            j.forEach(k => {
+                const pair = [ar[i], k];
+                result.push(pair);
+            });
+        }
+    }
+    return result.length;
+}
+
+const input1 = {
+    n: 6,
+    k: 3,
+    ar: [1, 3, 2, 6, 1, 2],
+}
+
+const input2 = {
+    n: 100,
+    k: 22,
+    ar: [43, 95, 51, 55, 40, 86, 65, 81, 51, 20, 47, 50, 65, 53, 23, 78, 75, 75, 47, 73, 25, 27, 14, 8, 26, 58, 95, 28, 3, 23, 48, 69, 26, 3, ,73, 52, 34, ,7, 40, 33,
+        56, 98, 71, 29, 70, 71, 28, 12, 18, 49, 19, 25, 2, 18, 15, 41, 51, 42, 46, 19, 98, 56, 54, 98, 72, 25, 16, 49, 34, 99, 48, 93, 64, 44, 50, 91, 44, 17, 63, 27, 
+        3, 65, 75, 19, 68, 30, 43, 37, 72, 54, 82, 92, 37, 52, 72, 62, 3, 88, 82, 71]
+}
+
+console.log(divisibleSumPairs(input2.n, input2.k, input2.ar));
